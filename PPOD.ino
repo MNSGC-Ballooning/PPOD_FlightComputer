d80160ea5b21d6fda9390969ea7887a275d202b3
AndrewVanGerpen <47224205+AndrewVanGerpen@users.noreply.github.com>
2019-07-09 09:57:20 -0500

    Add files via upload

--- PPOD.ino
+++ PPOD.ino
@@ -1,77 +1,65 @@
-#include "src\Smart.h"
+#include <UbloxGPS.h>
+#include <TinyGPS++.h>
 #include <SoftwareSerial.h>
-#include <Relay_XBee.h>
+#include <RelayXBee.h>
+#include "src\Smart.h"
 
 #define smartPin 10
-#define light 9 
-#define onlightn 7 //The light that turns on when it is powered up and remains on
-#define onlightp 6 // See above
+#define onlight 7
+#define temp_pin A0 //The light that turns on when it is powered up and remains on
 
+bool fix = false;
 bool released = false;
-String ID = "SOC";
+String ID = "POD";
 String command;
-float cutTime = 70; //Time in minutes
+float cutTime = 70; //Time in minutes is this right??? 70,000 feet approx.
 float cutAlt = 18288; //Alt in meters
 float currentAlt = 0;
+String rawGPS;
 
-SoftwareSerial ss = SoftwareSerial(2,3);
-XBee xBee = XBee(&ss, ID);
-
+SoftwareSerial ss = SoftwareSerial(6,7);
+SoftwareSerial ssGPS = SoftwareSerial(2,3);
+RelayXBee xBee = RelayXBee(&ss, ID);
+TinyGPSPlus gps;
 Smart smart = Smart(smartPin);
 
 void setup() {
-  smart.initialize();
-  //gps.initialize();
-  //pinMode(light, OUTPUT);
-  pinMode(onlightn, OUTPUT);
-  pinMode(onlightp, OUTPUT);
-  ss.begin(9600);
-  //Serial.println("Testing");
-  digitalWrite(onlightn, LOW);
-  digitalWrite(onlightp, HIGH);
+  Serial.begin(9600);
+  //smart.initialize();
+  ssGPS.begin(UBLOX_BAUD);
+  Serial.println("GPS serial set");
+  //gps.init();
+  Serial.println("GPS initialized");
+  //delay(50);
+  //gps.setAirborne();
+  //(gps.setAirborne()){Serial.println("Air mode successfully set");}
+  pinMode(onlight, OUTPUT);
+  ss.begin(XBEE_BAUD);
+  xBee.init('A');
+  Serial.println("Xbee initialized");
+  digitalWrite(onlight, LOW);
+
+  String header = "GPS Time,Lat,Lon,Alt (m),# Sats, temp(C), seconds since bootup ";
+  Serial.println(header);
 }
 
 void loop() {
-  //gps.update();
-  if (released == false) {
-    digitalWrite(onlightn,HIGH);
-    digitalWrite(onlightp,LOW);
-    delay(1500);
-    digitalWrite(onlightn,LOW);
-    digitalWrite(onlightp,HIGH);
-    delay(1500);
-  }
-
-  if (!released && ((millis()/60000.0 > cutTime-.2)) || (currentAlt < cutAlt)) {
-    digitalWrite(light, HIGH);}
+  updateXbee();
+  logData();
   if (!released && ((millis()/60000.0 > cutTime)) || (currentAlt >= cutAlt)) {
-    //ss.println("PPOD: Releasing");
-    int counter = 0;
-    while (counter < 16) {
-      digitalWrite(onlightp, HIGH);
-      digitalWrite(onlightn, LOW);
-      delay(200);
-
-      counter += 1;
-    }
+     smart.release();
+     released = true;
+     ss.println("PPOD released on Timer");
+  }
+  if (released == true) {digitalWrite(onlight,HIGH);}
   
-    smart.release();
-    
-    released = true;
+  delay(500);
+  digitalWrite(onlight,LOW);
+  delay(500);
+}
 
-    //ss.println("PPOD released");
-  }
-
-  if (released == true) {
-    digitalWrite(onlightn, HIGH);
-    digitalWrite(onlightp, LOW);
-    delay(500);
-    digitalWrite(onlightn, LOW);
-    digitalWrite(onlightp, HIGH);
-    delay(500);
-  }
-
-  if (ss.available() > 0) {
+void updateXbee(){
+if (ss.available() > 0) {
     command = ss.readString();
     if (command == "timeleft") {
       ss.print(",,,,,,,,,,,,PPOD ," + String(cutTime - millis()/60000));
@@ -124,10 +112,32 @@ void loop() {
       else if (released == true) {
         ss.print(",PPOD:, Released");
       }
-    }
-    else if (command == "humidity" || command == "pressure" || command == "1u-temp" || command == "1U Data") {
-      return;
-    }
-  }
+    }}}
+
+double temperature(){
+  double rawTemp = analogRead(temp_pin);
+  double tempV = rawTemp*(5.0/1024.0);
+  double temp = ((tempV-0.5)*100);
+  return temp;
 }
 
+void logData(){
+
+  int seconds = (millis()/1000)%60;
+  int minutes = (millis()/60000)%60;
+  int hours = (millis()/1000)/3600;
+  
+  String data = String(gps.time.hour()) + ":" + String(gps.time.minute()) + ":" + String(gps.time.second()) + ", "
+                + String(gps.location.lat(), 4) + ", " + String(gps.location.lng(), 4) + ", " + String(gps.altitude.meters(), 4) +  ", " 
+                + String(temperature()) + ", " + String(released) + ", " + String(hours) + ":" + String(minutes) + ":"
+                + String(seconds);
+
+  /*if (gps.getFixAge() > 2000){
+    fix = false;
+    data += ", no fix";}
+  else{
+    fix = true;
+    data+= ", fix";}
+    */
+  Serial.println(data);
+}

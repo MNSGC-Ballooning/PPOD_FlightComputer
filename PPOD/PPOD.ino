4671446882ab0c4b392a15a16e6a2c3192fb2d13
ZoeFoster <50996111+ZoeFoster@users.noreply.github.com>
2019-08-15 13:48:31 -0500

    Add files via upload

--- PPOD/PPOD.ino
+++ PPOD/PPOD.ino
@@ -4,13 +4,12 @@
 #include <RelayXBee.h>
 #include <SD.h>
 #include <SPI.h>
-#include <Smart.h>
+#include "src/Smart.h"
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
 //#define chipSelect 53 //use this for Mega board
-#define smartPin 2
-#define onlight 7
+#define smartPin 3
 #define temp_pin 4
 #define press_pin A1
 #define gps_status_pin A2 //blue, flashes every 10 seconds if fix, every second if not
@@ -19,32 +18,29 @@
 #define GPS_serial Serial1
 #define XBee_serial Serial3
 
-
 File datalog;
 char filename[] = "SDPPOD00.csv"; //File name template
 bool SDactive = false;
 
 String data;
 bool fix = false;
+bool attempt = false;
 bool released = false;
 String ID = "POD";
 String command;
-float cutTime = 70.0; //Time in minutes is this right??? 70,000 feet approx.
+float cutTime = 70.0; //Time in minutes, 70,000 feet approx.
 float cutAlt = 18288; //Alt in meters
 float currentAlt = 0;
 String rawGPS;
 short status_counter = 0;
-bool status_engaged = false;
 unsigned long int radioTime = 0;
 const int chipSelect = BUILTIN_SDCARD; //for Teensy only
 
-//SoftwareSerial XBee_serial = SoftwareSerial(6,7);
-//SoftwareSerial XBee_serialGPS = SoftwareSerial(2,3);
 RelayXBee xBee = RelayXBee(&XBee_serial, ID);
 UbloxGPS ublox(&GPS_serial);
 Smart smart = Smart(smartPin);
-OneWire oneWire(temp_pin);
-DallasTemperature digital_temp(&oneWire);
+OneWire oneWire(temp_pin); //For Dallas sensor
+DallasTemperature digital_temp(&oneWire); //For Dallas sensor
 
 void setup() {
   pinMode(gps_status_pin, OUTPUT);
@@ -53,11 +49,10 @@ void setup() {
   Serial.begin(9600);
   smart.initialize();
   digital_temp.begin();
-  pinMode(onlight, OUTPUT);
+  pinMode(10, INPUT_PULLUP);
   XBee_serial.begin(XBEE_BAUD);
   xBee.init('A');
   Serial.println("Xbee initialized");
-  pinMode(10,OUTPUT);
   GPS_serial.begin(UBLOX_BAUD);
   ublox.init();
   delay(200);
@@ -67,19 +62,20 @@ void setup() {
     i++;
     if (ublox.setAirborne()) {
       Serial.println("Air mode successfully set.");
-      break;}}
+      break;}
+    if (i==50) Serial.println("Failed to set to air mode.");
+  }
   Serial.println("GPS configured");
   Serial.print("Initializing SD card...");
   pinMode(chipSelect,OUTPUT);
   if(!SD.begin(chipSelect)){
     Serial.println("Card failed, or not present");
-    while(millis()<30000)
-    {
+    while(millis()<30000) {
     digitalWrite(sd_status_pin,HIGH);
     delay(200);
     digitalWrite(sd_status_pin,LOW);
-    delay(200);
-    }}
+    delay(200);}
+  }
   else {
     Serial.println("Card initialized.\nCreating File...");
     for (byte i = 0; i < 100; i++) {
@@ -89,141 +85,127 @@ void setup() {
         datalog = SD.open(filename, FILE_WRITE);
         SDactive = true;
         Serial.println("Logging to: " + String(filename));
-        break;
-      }
+        break;}
     }
     if (!SDactive) Serial.println("No available file names; clear SD card to enable logging");
   }
-  digitalWrite(onlight, LOW);
-
-  String header = "GPS Time,Lat,Lon,Alt (m), temp(C), pressure(PSI), released? , time since bootup ";
-  //String header = "GPS Time,Lat,Lon,Alt (m),# Sats, temp(C), seconds since bootup ";  //old version
+  String header = "GPS Time, Lat, Lon, Alt (ft), temp(C), pressure(PSI), attempted?, released?, time since bootup ";
   Serial.println(header);
-  if(SDactive) {
-    datalog = SD.open(filename, FILE_WRITE);
-    datalog.println(header);
-    datalog.close();
-  }
+  if(SDactive) logData(header);
 }
 
 void loop() {
   ublox.update();
   if (millis()%1000 == 500) {
+    released = !digitalRead(10);
     updateXbee();
-    logData();
+    updateData();
     updateStatus();
-    if (!released && ((millis()/60000.0 > cutTime) || (currentAlt >= cutAlt))) {
-       smart.release();
-       released = true;
-       XBee_serial.println("PPOD released on Timer");
-       if(SDactive) datalog.println("PPOD released on Timer");
+    if (!released) {
+      float previousAlt = currentAlt;
+      currentAlt = ublox.getAlt_meters();
+      if (millis()/60000.0 > cutTime) {
+        smart.release();
+        attempt = true;
+        xBee.send("PPOD attempted release on timer");
+        if (SDactive) logData("PPOD attempted release on timer");}
+      else if ((currentAlt >= cutAlt) && ((currentAlt-previousAlt) < 500)) {
+        smart.release();
+        attempt = true;
+        xBee.send("PPOD attempted release at altitude " + String(currentAlt) + " m");
+        if(SDactive) logData("PPOD attempted release at altitude " + String(currentAlt) + " m");}
     }
-    if (released == true) {digitalWrite(onlight,HIGH);}
+    delay(100);
+    updateStatus();
   }
-  if (millis()%1000 == 0) {
-    digitalWrite(onlight,LOW);
-    updateStatus();}
+}
+
+void logData(String dataa) {
+  datalog = SD.open(filename, FILE_WRITE);
+  datalog.println(dataa);
+  datalog.close();
 }
 
 void updateXbee(){
-if (XBee_serial.available() > 0) {
+  if (XBee_serial.available() > 0) {
     command = xBee.receive();
     if (command.startsWith("T")) {
-      XBee_serial.print(",,,,,,,,,,,,PPOD ," + String(cutTime - millis()/60000));
+      xBee.send(String(cutTime - millis()/60000));
     }
     else if (command.startsWith("+")) {
       command.remove(0,1);
       float addtime = command.toFloat(); 
       cutTime = cutTime + addtime;
-      XBee_serial.print(",,,,,,,,,,,,PPOD: " + String(addtime) + " Minutes added." + " New Time Left: ," + String(cutTime - millis()/60000));
-      if(SDactive) datalog.println(",,,,,,,,,,,,PPOD: " + String(addtime) + " Minutes added." + " New Time Left: ," + String(cutTime - millis()/60000));
+      xBee.send(String(addtime) + " Minutes added." + " New Time Left: ," + String(cutTime - millis()/60000));
+      if(SDactive) logData(String(addtime) + " Minutes added." + " New Time Left: ," + String(cutTime - millis()/60000));
     }
     else if (command.startsWith("-")) {
       command.remove(0,1);
       float subtime = command.toFloat();
       cutTime = cutTime - subtime;
-      XBee_serial.print(",,,,,,,,,,,,PPOD " + String(subtime) + " Minutes removed." + " New Time Left: ," + String(cutTime - millis()/60000));
-      if(SDactive) datalog.println(",,,,,,,,,,,,PPOD " + String(subtime) + " Minutes removed." + " New Time Left: ," + String(cutTime - millis()/60000));
+      xBee.send(String(subtime) + " Minutes removed." + " New Time Left: ," + String(cutTime - millis()/60000));
+      if(SDactive) logData(String(subtime) + " Minutes removed." + " New Time Left: ," + String(cutTime - millis()/60000));
     }
     else if (command.startsWith("C")) {
       if (released == true) {
-        XBee_serial.print(",,,,,,,,,,,,PPOD: Already Released");
-      }
+        xBee.send("PPOD already released");}
       if (released == false) {
         smart.release();
-        released = true;
-        XBee_serial.print(",,,,,,,,,,,,PPOD: Released");
-        if(SDactive) datalog.println(",,,,,,,,,,,,PPOD: Released via command");
-      }
+        attempt = true;
+        xBee.send("PPOD attempted release");
+        if(SDactive) logData("PPOD attempted release via command");}
     }
     else if (command.startsWith("A")) {
       if (released == false) {
-      XBee_serial.print(data);//",PPOD:, " + String(cutTime - millis()/60000) + "," + String(cutAlt - currentAlt));
-      }
+        xBee.send(data);}
       else if (released == true) {
-        XBee_serial.print(",PPOD:, Released");
-      }
-    }}}
+        xBee.send("Released");}
+    }
+  }
+}
 
-float temperature(){
+float temperature() {
   digital_temp.requestTemperatures();
   float temp = digital_temp.getTempCByIndex(0);
   return temp;
 }
 
-float pressure(){
+float pressure() {
   float rawPress = analogRead(press_pin);
   float pressV = rawPress*(5.0/1024);
   float pressure = ((pressV - 0.5)*(15.0/4.0));
   return pressure;
 }
 
-void logData(){
-
+void updateData() {
   short seconds = (millis()/1000)%60;
   short minutes = (millis()/60000)%60;
   short hours = (millis()/1000)/3600;
   
   data = String(ublox.getHour()-5) + ":" + String(ublox.getMinute()) + ":" + String(ublox.getSecond()) + ", "
                 + String(ublox.getLat(), 4) + ", " + String(ublox.getLon(), 4) + ", " + String(ublox.getAlt_feet(), 4) +  ", "
-                + String(temperature()) + ", " + String(pressure()) + ", " + String(released) + ", " + String(hours) + ":" + String(minutes) + ":"
-                + String(seconds);
-              
-  /*if (gps.getFixAge() > 2000){
-    fix = false;
-    data += ", no fix";}
-  else{
-    fix = true;
-    data+= ", fix";}
-    */
+                + String(temperature()) + ", " + String(pressure()) + ", " + String(attempt) + ", " + String(released) + ", "
+                + String(hours) + ":" + String(minutes) + ":" + String(seconds);
+  if(ublox.getFixAge() > 2000) fix = false;
+  else fix = true;
   Serial.println(data);
-  if(radioTime + 10000 < millis()){
+  if(radioTime + 10000 < millis()) {
     xBee.send(data);
-    radioTime = millis();}
-  
-  if(SDactive){
-    datalog = SD.open(filename, FILE_WRITE);
-    datalog.println(data);
-    datalog.close();
-    digitalWrite(sd_status_pin, HIGH);
-  }
+    radioTime = millis();} 
+  if(SDactive) {
+    logData(data);
+    digitalWrite(sd_status_pin, HIGH);}
 }
 
-void updateStatus()
-{
-  if(status_engaged)
-  {
+void updateStatus() {
+  if(status_counter%2 == 1) {
     digitalWrite(gps_status_pin, LOW);
     digitalWrite(smart_status_pin, LOW);
-    if(SDactive)digitalWrite(sd_status_pin, LOW);
-  }
-  else
-  {
-    status_counter ++;
-    if(status_counter > 10) status_counter = 1;
-    if((fix && status_counter==10) || !fix) digitalWrite(gps_status_pin, HIGH);
-    if((!released && status_counter==10) || released) digitalWrite(smart_status_pin, HIGH);
-    if(!SDactive) digitalWrite(sd_status_pin, HIGH);
-  }
-  status_engaged = !status_engaged;
+    if(SDactive)digitalWrite(sd_status_pin, LOW);} 
+  else {
+    if((fix && status_counter==20) || !fix) digitalWrite(gps_status_pin, HIGH);
+    if((!released && status_counter==20) || released) digitalWrite(smart_status_pin, HIGH);
+    if(!SDactive) digitalWrite(sd_status_pin, HIGH);}
+  status_counter ++;
+  if(status_counter > 20) status_counter = 1;
 }
